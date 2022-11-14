db1128485cb253f1445cc31d863d8dcf14149c2a
Andreea Pocol <andreeapocol@ymail.com>
2022-11-14 14:32:55 -0500

    Put papers into year bins

--- deepfake_research.py
+++ deepfake_research.py
@@ -10,3 +10,24 @@ if result.reason != "OK":
 
 paper_records = result.json()
 hits = paper_records["data"]
+papers_by_year = {}
+
+for hit in hits:
+    result = requests.get(
+        "https://api.semanticscholar.org/graph/v1/paper/{id}?fields=url,year,title".format(
+            id=hit["paperId"]
+        )
+    )
+
+    if result.reason != "OK":
+        print(result.status_code, result.reason)
+    exit
+
+    paper_info = result.json()
+    year = paper_info["year"]
+    if year in papers_by_year:
+        papers_by_year[year] += 1
+    else:
+        papers_by_year[year] = 1
+
+    print(papers_by_year)
