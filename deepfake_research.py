fb8e8897aa81b88905e6756577b9975be1e1a694
Andreea Pocol <andreeapocol@ymail.com>
2022-11-17 15:16:27 -0500

    added ethical consideration

--- deepfake_research.py
+++ deepfake_research.py
@@ -14,9 +14,32 @@ def writeToFile(papers_by_year, save_path):
             file.write("%s, %s\n" % (key, papers_by_year[key]))
 
 
-def fetchPaperData():
+def fetchDeepfakeAndEthicsPapers():
     papers_by_year = {}
-    for year in range(2019, 2023):
+    for year in range(2000, 2023):
+        result = requests.get(
+            "http://api.semanticscholar.org/graph/v1/paper/search?query=deepfake+ethic&year={}".format(
+                year
+            )
+        )
+
+        if result.reason != "OK":
+            print("Error: ", result.status_code, result.reason)
+            exit
+
+        paper_records = result.json()
+        num_papers = paper_records["total"]
+        print(
+            "Total number of deepfake+ethic papers in {}: {}".format(year, num_papers)
+        )
+        papers_by_year[year] = num_papers
+
+    return papers_by_year
+
+
+def fetchDeepfakePapers():
+    papers_by_year = {}
+    for year in range(2000, 2023):
         result = requests.get(
             "http://api.semanticscholar.org/graph/v1/paper/search?query=deepfake&year={}".format(
                 year
@@ -29,7 +52,7 @@ def fetchPaperData():
 
         paper_records = result.json()
         num_papers = paper_records["total"]
-        print("Total number of papers in {}: {}".format(year, num_papers))
+        print("Total number of deepfake papers in {}: {}".format(year, num_papers))
         papers_by_year[year] = num_papers
 
         return papers_by_year
@@ -47,7 +70,12 @@ def plotData(file):
     fig.show()
 
 
-file = "num_deepfake_papers_by_year.csv"
-# papers_by_year = fetchPaperData()
-# writeToFile(papers_by_year, save_path=file)
-plotData(file)
+deepfake_file = "num_deepfake_papers_by_year.csv"
+deepfake_ethics_file = "num_deepfake_ethics_papers_by_year.csv"
+
+# deepfake_papers_by_year = fetchDeepfakePapers()
+deepfake_ethics_papers_by_year = fetchDeepfakeAndEthicsPapers()
+# writeToFile(deepfake_papers_by_year, save_path=deepfake_file)
+writeToFile(deepfake_ethics_papers_by_year, save_path=deepfake_ethics_file)
+# plotData(deepfake_papers_by_year)
+plotData(deepfake_ethics_papers_by_year)
